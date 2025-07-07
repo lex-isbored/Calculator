7f58b1df85bd6352f92e3a667a1f0b514ebd7cd7
Lex <leireasensiogarcia@gmail.com>
2025-07-07 17:50:11 +0200

    Asking order, can't divide by 0
    I changed that if the option you choose is not valid (not from 1 to 4) then it says it's not valid isntead of asking for two numbers and then saying it's not valid.
    
    I changed that when you choose divide, you can't divide by 0.
    
    I also added the versions list to the README

--- 2.Calculadora chafa.py
+++ 2.Calculadora chafa.py
@@ -1,23 +1,28 @@
-    #menu
+# Menu
 print("Welcome!")
 print("Introduce one of the following options typing its corresponding number :)")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
 
-    #choose number
+# Choose option
 option = int(input("Choose an option: "))
-num1 = float(input("Introduce the first number: "))
-num2 = float(input("Introduce the second number: "))
 
-if option == 1:
-     print("Result: ", num1 + num2)
-elif option == 2:
-     print("result: ", num1 - num2)
-elif option == 3:
-    print("Result: ", num1 * num2)
-elif option == 4:
-    print("Result: ", num1 / num2)
+if option not in [1, 2, 3, 4]:
+    print("Not valid")
 else:
-     print("Not valid")
+    num1 = float(input("Introduce the first number: "))
+    num2 = float(input("Introduce the second number: "))
+
+    if option == 1:
+        print("Result: ", num1 + num2)
+    elif option == 2:
+        print("Result: ", num1 - num2)
+    elif option == 3:
+        print("Result: ", num1 * num2)
+    elif option == 4:
+        if num2 == 0:
+            print("You cannot divide by zero!")
+        else:
+            print("Result: ", num1 / num2)
--- README.md
+++ README.md
@@ -1,12 +1,21 @@
 ## Description
-This project is a calculator I made using Python. 
+
+This project is a calculator I made using Python.
 You can add, subtract, multiply or divide.
 You can only write two numbers.
 
 ## Instructions
-- Open the program
-- Select an option typing a number from 1 to 4
-- Enter two different numbers
-- The calculator will give you the result
+
+* Open the program
+* Select an option typing a number from 1 to 4
+* Enter two different numbers
+* The calculator will give you the result
 
   Made by Leire
+
+  
+
+  ## Versions
+
+* V1.0 Created a calculator in which you can type two numbers, and add, subrtact, multiply or divide them
+* V1.1 Changed that now if you choose an option which is not valid, it Will say it's not valid instead of asking for the numbers. Now it also tells you that you can't divide by 0
